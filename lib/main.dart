0eac59e46fa46db32dc16eaf0a9abbc0d86e8d3d
isuru-liyanage <isuruliyanageofficial@gmail.com>
2024-03-12 03:34:49 +0530

    login  page ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jobseeker_madhack/login.dart';
 import 'package:jobseeker_madhack/signup.dart';
 
 void main() {
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  SignupPage(),
+      home:  LoginPage(),
     );
   }
 }
