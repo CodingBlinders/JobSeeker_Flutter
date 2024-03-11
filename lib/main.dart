15df3ca59d931e0d18c26d61c91b749b2c9732c3
isuru-liyanage <isuruliyanageofficial@gmail.com>
2024-03-12 03:25:47 +0530

    Sign up page ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jobseeker_madhack/signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  SignupPage(),
     );
   }
 }
